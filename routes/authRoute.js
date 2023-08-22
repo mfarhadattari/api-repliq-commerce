9d0c066517c2e3f1f10c5858002b475484b26021
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-22 23:37:46 +0600

    Create account api implemented

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,9 +2,24 @@
 
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send("This is auth route!");
+// ! -------------------- CREATE ACCOUNT --------------
+router.post("/create-account", async (req, res) => {
+  const userCollection = req.userCollection;
+  const { userName, userPhone, avatar, password } = req.body;
+  const alreadyExist = await userCollection.findOne({ userPhone });
+  if (alreadyExist) {
+    return res.send({ alreadyAccount: true });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const result = await userCollection.insertOne({
+    userName,
+    userPhone,
+    avatar,
+    password: hashedPassword,
+  });
+  res.send(result);
 });
 
 module.exports = router;
