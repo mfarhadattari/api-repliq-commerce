9b24deab64bba9d852b183b299d0ec0a02781530
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-22 22:39:44 +0600

    PRODUCTS DETAILS API implemented

--- routes/publicRoute.js
+++ routes/publicRoute.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
+const { ObjectId } = require("mongodb");
 
 // ! ------------------ PRODUCTS API -----------------
 router.get("/products", async (req, res) => {
@@ -19,6 +20,16 @@ router.get("/popular-products", async (req, res) => {
     .limit(6)
     .toArray();
   res.send(popularProducts);
+});
+
+// ! ------------------ PRODUCTS DETAILS API---------------
+router.get("/products/:id", async (req, res) => {
+  const productCollection = req.productCollection;
+  const id = req.params.id;
+  console.log(id);
+  const product = await productCollection.findOne({ _id: new ObjectId(id) });
+  console.log(product);
+  res.send(product);
 });
 
 module.exports = router;
