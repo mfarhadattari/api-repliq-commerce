afbaec7d8f77b504faca3433294dfa8751dcd7c3
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-23 21:12:42 +0600

    delete product api implemented

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,12 +2,21 @@
 
 const express = require("express");
 const router = express.Router();
+const {ObjectId} = require("mongodb")
 
 // ! ------------------ PRODUCTS ---------------
 router.get("/products", async (req, res) => {
   const productCollection = req.productCollection;
   const products = await productCollection.find().toArray();
   res.send(products.reverse());
+});
+
+// ! ------------------ Delete Product ---------------
+router.delete("/delete-product/:id", async (req, res) => {
+  const productCollection = req.productCollection;
+  const id = req.params.id;
+  const result = await productCollection.deleteOne({ _id: new ObjectId(id) });
+  res.send(result);
 });
 
 module.exports = router;
